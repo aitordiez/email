511459053a123035e1938e55dd13b14b7d207baa
Aitor Diez Fernandez <aitorchic@gmail.com>
2017-11-19 11:29:57 +0100

    te muestra el total de enviados, recibidos, caacteres y la direccion larga

--- MailClient.java
+++ MailClient.java
@@ -11,7 +11,16 @@ public class MailClient
     private MailServer server;
     // The user running this client.
     private String user;
-
+    //mensajes enviados.
+    private int entregados;
+    //mensajes recibidos.
+    private int recibidos;
+    //mensaje
+    private String mensaje;
+    //el numero de caracteres 
+    private int caracteres;
+    //contador
+    private int enviados;
     /**
      * Create a mail client run by user and attached to the given server.
      */
@@ -19,20 +28,21 @@ public class MailClient
     {
         this.server = server;
         this.user = user;
+        entregados= 0;
+        recibidos = 0;
+        mensaje = mensaje;
+        caracteres = 0;
+        this.enviados = 0;
     }
-
+    
     /**
-     * Return the next mail item (if any) for this user.
+     * 
      */
     public MailItem getNextMailItem()
     {
         return server.getNextMailItem(user);
     }
 
-    /**
-     * Print the next mail item (if any) for this user to the text 
-     * terminal.
-     */
     public void printNextMailItem()
     {
         MailItem item = server.getNextMailItem(user);
@@ -45,6 +55,32 @@ public class MailClient
     }
 
     /**
+     * Funcionalidad 05 Aitor Diez
+     */
+    public void datosmensajes()
+    {
+        System.out.println("entregados: " + enviados);
+        System.out.println("recibidos: " + server.howManyMailItems(user));
+        MailItem emailmaslargo = getNextMailItem();
+        boolean quedanEmails = true;
+        while(quedanEmails){
+            MailItem emailservidor = getNextMailItem();
+                if (emailservidor == null){
+                    quedanEmails = false;
+                }
+                else{
+                    if(emailmaslargo.getMessage().length()>=emailservidor.getMessage().length()){
+                emailmaslargo=emailservidor;
+            }
+   
+        }
+    
+    }
+    
+        System.out.println("Caracteres mas largo: " + emailmaslargo.getMessage().length());
+        System.out.println("direccion" + emailmaslargo.getFrom());
+  }
+    /**
      * Send the given message to the given recipient via
      * the attached mail server.
      * @param to The intended recipient.
@@ -52,7 +88,8 @@ public class MailClient
      */
     public void sendMailItem(String to,String subject, String message)
     {
-        MailItem item = new MailItem(user, to, message,  subject);
+        MailItem item = new MailItem(user, to, subject, message);
         server.post(item);
+        enviados += 1;
     }
 }
